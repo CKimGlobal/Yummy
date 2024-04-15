a0739c7bd1dbdab6cd6553aaac887e3ab6bf8b0c
C. Kim <ckimglobal@gmail.com>
2024-04-15 22:57:48 +0900

    Make welcome page

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <div className="Header-container">
+                <button className="Menu-button">B</button>
+                <div className="Info-icon">I</div>
+                <div className="Refresh-icon">R</div>
+            </div>
         </div>
     );
 }
